07ddba9a5e5484f55b3326d08c34d0fa30456658
HaronPaul <hplorente@up.edu.ph>
2021-08-20 00:18:55 +0800

    switched to new repo

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import NavigationBar from './components/navigationBar'
 import Home from './components/home/homepage'
 import Vaccinations from './components/vaccinations/vaccinations';
 import Regional from './components/regional/regional'
+import NewsPage from './components/news/news';
 import Footer from './components/footer'
 import InfoPage from './components/informationPage/infoPage'
 
@@ -27,11 +28,12 @@ function App() {
                     {/* <Route path="*" render={() => <Redirect to="/" />} /> */}
               <Route exact path="/vaccinations" component={Vaccinations}/>
               <Route exact path="/regional" component={Regional}/>
+              <Route exact path="/news" component={NewsPage}/>
               <Route exact path="/info" component={InfoPage}/>
+
             </Switch>
         
         </BrowserRouter>
-        
     </div>
   );
 }

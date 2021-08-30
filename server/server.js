e8db0ed2dae1f2604ce57047828e25e0372967be
collinsritch <collinsritch@gmail.com>
2021-08-30 21:04:22 +0800

    build errors emulate cors

--- server/server.js
+++ server/server.js
@@ -13,7 +13,12 @@ dotenv.config();
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
+app.use(function(req,res,next) {
+    res.header("Acess-Control-Allow-Origin", "*");
+    next();
 });
 
 app.use(cors());
